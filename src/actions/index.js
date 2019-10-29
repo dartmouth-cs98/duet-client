7b120c55325d7a68348d8433f3f595612e87d316
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2019-10-29 17:06:49 -0400

    translate spotify data to our schema

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import * as types from '../constants/actionTypes';
 import SpotifyWebApi from 'spotify-web-api-js';
+import axios from 'axios';
 
 export const fetchUserData = (token, time_range) => {
     return (dispatch) => {
@@ -10,7 +11,17 @@ export const fetchUserData = (token, time_range) => {
         const spotifyApi = new SpotifyWebApi();
         spotifyApi.setAccessToken(token);
 
-        // TODO: get user profile
+        // get user profile
+        axios({
+            method: 'get', 
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+              Authorization: 'Bearer ' + token,
+            }
+        })
+        .then((response) => {
+            dispatch({ type: types.FETCH_USER_PROFILE, profile: response.data })
+        })
         
         // get top tracks
         spotifyApi.getMyTopTracks({limit: 50, time_range})
