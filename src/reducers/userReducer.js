60e39387a08ca39bfe910f603fd61f7861cd9aeb
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2019-11-02 14:15:04 -0400

    genreChart reflects user data

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -3,31 +3,23 @@ import initialState from './initialState';
 
 const userReducer = (state = initialState.user, action) => {
   switch(action.type) {
-    case types.FETCH_USER_TOP_TRACKS:
-      return {...state, topTracks: action.tracks}
-    case types.FETCH_USER_TOP_ARTISTS:
-      return {...state, topArtists: action.artists}
     case types.FETCH_USER_PROFILE: {
       const { 
         id,
-        country, 
         display_name, 
         email, 
-        followers, 
         images, 
-        product, 
-        uri
       } = action.profile;
-      return {...state, id, country, display_name, email, followers, images, product, uri}
+      return {...state, id, display_name, email, images }
     }
     case types.FETCH_USER_TASTE:
       return { ...state, taste: action.taste, avg_taste: action.avg_taste }
     case types.FETCH_TOP_GENRES:
       return { ...state, genre_counts: action.genre_counts }
-    case types.FETCH_TOP_DECADES:
-      return { ...state, decade_counts: action.decade_counts}
-    case types.FETCH_TRACK_POPULARITIES:
-      return { ...state, popularities: action.popularities }
+    case types.FETCH_DECADE:
+      return { ...state, decade: action.decade}
+    case types.FETCH_POPULARITY:
+      return { ...state, popularity: action.popularity }
     default:
       return state
   }
