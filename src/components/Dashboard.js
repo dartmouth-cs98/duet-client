62537d917d1dfd97ae2324112f173ebf8ac7da9c
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-11-02 12:32:56 -0400

    sidebar with dummy data done

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Sidebar'
+
+const NotFoundPage = () => {
+  return (
+    <div className="Dashboard">
+      <Sidebar/>
+    </div>
+  );
+};
+
+export default NotFoundPage;
