d4f68cb1b5487be2b1d0b40e1d28217059156b2e
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2019-11-12 16:51:32 -0500

    user updates in database everytime

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,6 @@ import * as types from '../constants/actionTypes';
 import SpotifyWebApi from 'spotify-web-api-js';
 import { getCurrentUserProfile, getTrackInfos, getTaste, getAvgTaste, getGenreCount } from '../utils/spotifyUtils';
 import { getUser, addUser, addNewFriend } from '../utils/backendUtils';
-import _ from 'lodash';
 
 export const fetchUserData = (token, time_range) => {
     return (dispatch) => {
@@ -30,41 +29,22 @@ export const fetchUserData = (token, time_range) => {
             dispatch({ type: types.FETCH_TRENDEX, trendex: popularity });
 
             const audioFeaturesPromise = spotifyApi.getAudioFeaturesForTracks(trackIds);
-            // .then((tracks) => {
-            //     dispatch({ 
-            //         type: types.FETCH_USER_TASTE, 
-            //         taste: getTaste(tracks.audio_features), 
-            //         avg_taste: getAvgTaste(tracks.audio_features) 
-            //     });
-            // })
-
+        
             // TOP ARTISTS STUFF
             const genreCounts = getGenreCount(topArtists.items);
             dispatch({ type: types.FETCH_TOP_GENRES, genre_counts: genreCounts })
 
+            // TODO: get rid of name once it's no longer required by backend
+            audioFeaturesPromise.then((tracks) => {
+                const taste =  getTaste(tracks.audio_features);
+                const avg_taste = getAvgTaste(tracks.audio_features);
+                dispatch({ type: types.FETCH_USER_TASTE, taste, avg_taste });
+                addUser({ country, display_name, name: display_name, id, email, trendex: popularity, decade, taste, avg_taste, genre_counts: genreCounts })
+            })   
+
             // check if user exists in database
-             getUser(id).then((user) => {
-                // returns empty object if user doesn't exist
-                if (_.isEmpty(user)) {
-                    // TODO: get rid of name once it's no longer required by backend
-                    audioFeaturesPromise.then((tracks) => {
-                        const taste =  getTaste(tracks.audio_features);
-                        const avg_taste = getAvgTaste(tracks.audio_features);
-                        dispatch({ type: types.FETCH_USER_TASTE, taste, avg_taste });
-                        addUser({ country, display_name, name: display_name, id, email, trendex: popularity, decade, taste, avg_taste })
-                    })     
-                    console.log('adding user', country, display_name, email, id);
-                }
-                else {
-                    audioFeaturesPromise.then((tracks) => {
-                        dispatch({ 
-                            type: types.FETCH_USER_TASTE, 
-                            taste: getTaste(tracks.audio_features), 
-                            avg_taste: getAvgTaste(tracks.audio_features) 
-                        });
-                    })    
-                    dispatch({ type: types.FETCH_USER_FRIENDS, friends: user.friends });
-                }
+            getUser(id).then((user) => {
+                dispatch({ type: types.FETCH_USER_FRIENDS, friends: user.friends });
             })
         });
     }
