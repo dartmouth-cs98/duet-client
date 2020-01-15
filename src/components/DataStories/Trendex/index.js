db8100be0c8db97d87c1b5072ab5367862b0384b
himadri narasimhamurthy <himadri99@gmail.com>
2020-01-14 21:42:03 -0500

    first pass trendex style

--- src/components/DataStories/Trendex/index.js
+++ src/components/DataStories/Trendex/index.js
@@ -4,7 +4,15 @@ import Page from '../../Page';
 const Trendex = () => {
     return (
         <Page background={'lightblue'} numPages={6} pageNum={1}>
-            Trendex
+            <div class="TrendexPage">
+                <h1>sig ep is trendier than u</h1>
+                <h2>78.0</h2>
+                <h3>sig ep's trendex</h3>
+                <div class="bottomSquare">
+                    <h2>39.0</h2>
+                    <h3>your trendex</h3>
+                </div>
+            </div>
         </Page>
     )
 }
