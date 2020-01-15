ac3971d58905e7d78103ee0178ec3cffce874f9b
himadri narasimhamurthy <himadri99@gmail.com>
2020-01-14 21:42:03 -0500

    first pass trendex style

--- src/components/DataStories/Trendex/index.js
+++ src/components/DataStories/Trendex/index.js
@@ -17,12 +17,20 @@ const Trendex = () => {
     }
 
     return (
-        <Page background={'lightblue'} numPages={6} pageNum={1}>
-            Trendex
-            <button id="share" onClick={() => saveScreen()}>...</button>
-
-            <div id="popup-background">
-                <div id="popup" onClick={() => handleClick()}>
+        <Page background={'#9BD6DC'} numPages={6} pageNum={1}>
+            <div className="TrendexPage">
+                <button id="share" onClick={() => saveScreen()}>...</button>
+                <div id="popup-background">
+                    <div id="popup" onClick={() => handleClick()}></div>
+                </div>
+                <h1><mark>sig ep is trendier than u</mark></h1>
+                <h2>78.0</h2>
+                <h3>sig ep's trendex</h3>
+                <div className="bottomSquare">
+                    <h1>\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\</h1>
+                    <h2>39.0</h2>
+                    <h3>your trendex</h3>
+                    <div id="arrow"></div>
                 </div>
             </div>
         </Page>
