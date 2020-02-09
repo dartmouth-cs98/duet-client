e953f41103bf9593d2f16cab22d4350399696cbb
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2020-02-09 12:49:42 -0500

    slight styling changes for decades

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,9 @@
 import { Route, Switch } from "react-router-dom";
-// import Login from "./Login";
-// import NotFoundPage from "./NotFoundPage";
 import PropTypes from "prop-types";
 import React from "react";
 import { hot } from "react-hot-loader";
 import AuthRedirect from './AuthRedirect';
-// import Survey from './Survey';
-// import Dashboard from "./Dashboard";
 import Story from './Story';
-// import { useSelector } from "react-redux";
-
-// This is a class-based component because the current
-// version of hot reloading won't hot reload a stateless
-// component at the top-level.
 
 const App = () => {
   return (
