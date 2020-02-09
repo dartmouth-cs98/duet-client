77b81d54b80c35d152a0d439de95c2789e562074
nicBerg <nicolas.j.bergen.20@dartmouth.edu>
2020-02-09 13:07:18 -0500

    top genres page- currently broken placement algo

--- src/components/DataStories/TopGenres/index.js
+++ src/components/DataStories/TopGenres/index.js
@@ -1,20 +1,65 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import Page from '../../Page';
 import html2canvas from 'html2canvas';
 
-const TopGenres = () => {
-    // const colors = ['#9BD6DC', '#212034', '#FFFFFF', '#E5277B'];
-    // const totalGenres = 48;  
-    // let percentages = [];
-    // let i = 0;
-    // topGenres.forEach((genre) => {
-    //     const percentage = (genre[1] / totalGenres) * 100;
-    //     percentages = [...percentages, {title: genre[0], value: percentage, color: colors[i % 4]}]
-    //     i ++;
-    // });
+const Bubbles = ({ topGenres, bubbleColor }) => {
+    const totalGenres = 48;
+    const R = 400;  
+    const t = 6.28318;
+    var bubbles = [];
+    topGenres.forEach((genre) => {
+        const percentage = (genre[1] / totalGenres);
+        bubbles = [...bubbles, {genre: genre[0], r: R*percentage, x: -R*percentage, y: -R*percentage}];
+    });
     
-    // // eslint-disable-next-line no-console
-    // console.log(percentages);
+    bubbles.sort(function(a, b) {return b.r - a.r})
+    // console.log(bubbles);
+
+    for (var i = 1; i < bubbles.length; i++){
+        var b1 = bubbles[i];
+        var b0 = bubbles[i-1];
+        //var a = Math.random()*t/3-(t/6);
+        var a = Math.random()*t;
+        b1.x = b0.x + (b0.r+b1.r*.8)*Math.cos(a);
+        b1.y = b0.y + (b0.r+b1.r*.8)*Math.sin(a);
+    }
+
+    // bubbles[0].x = -bubbles[0].r;
+    // bubbles[0].y = -bubbles[0].r;
+
+    var cx = 0;
+    var cy = 0;
+    bubbles.forEach((bubble) => {
+        cx += bubble.x;
+        cy += bubble.y;
+    });
+    cx /= bubbles.length;
+    cy /= bubbles.length;
+
+    bubbles.forEach((bubble) => {
+        // bubble.x -= cx;
+        // bubble.y -= cy;
+    });
+
+    return (
+        <div className="Bubbles">
+            {bubbles.map((bubble) => {
+                const { genre, r, x, y } = bubble;
+                return (
+                    <div className="Bubble" style={{ width: r*2, height: r*2, transform: `translate(${x}px, ${y}px)` }} >
+                        <h1 className="Bubble-Label" >{}</h1>
+                        <img src={bubbleColor} className="Bubble-Image"></img>    
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
+
+
+
+const TopGenres = ({ user_1, user_2 }) => {
 
     const saveScreen = () => {
         html2canvas(document.body).then(function(canvas) {
@@ -30,7 +75,7 @@ const TopGenres = () => {
     }
 
     return (
-        <Page background={'lightblue'} numPages={6} pageNum={2}>
+        <Page background={'#212034'} numPages={6} pageNum={2}>
             Top Genres
             <button id="share" onClick={() => saveScreen()}>...</button>
 
@@ -38,8 +83,20 @@ const TopGenres = () => {
                 <div id="popup" onClick={() => handleClick()}>
                 </div>
             </div>
+            <div className ="TopGenres-Page">
+                <div>
+                    <h1 className="TopGenres-Title">Top Genres</h1>
+                </div>
+                <Bubbles topGenres={user_1.genreCounts} bubbleColor={"/assets/bubblePink.png"}></Bubbles>
+                <Bubbles topGenres={user_1.genreCounts} bubbleColor={"/assets/bubbleBlue.png"}></Bubbles>
+            </div>
         </Page>
     )
 }
 
 export default TopGenres;
+
+
+
+
+
