af9957a57739ec432b85d84a33df77fea7ae1821
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2020-02-18 18:16:33 -0500

    joining gorup

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -15,20 +15,33 @@ const Stories = ({ history }) => {
     const { user_1, user_2, my_id } = useSelector((state) => state.users);
 
     if (user_1 && user_2) {
-        return (
-            <SwipeableViews>
-                <TopArists user_1={user_1} user_2={user_2} my_id={my_id} key="topartists"/>
-                <Trendex user_1={user_1} user_2={user_2} my_id={my_id} key="trendex"/>
-                <TopGenres user_1={user_1} user_2={user_2} key="topgenres"/>
-                <MusicalAttr user_1={user_1} user_2={user_2} my_id={my_id} key="musicalattr"/>
-                <Decades user_1={user_1} user_2={user_2} my_id={my_id} key="decades"/> 
-                <Members key="membersscreen"/>
-                <PlaylistGenerator key="playlistgenerator" user_1={user_1} user_2={user_2} my_id={my_id} />
-                <Share history={history} key="sharepage" />
-            </SwipeableViews>
-        );
-    } 
-    else {
+        if (user_2.isGroup && user_1.id == my_id) {
+            return (
+                <SwipeableViews>
+                    <TopArists user_1={user_1} user_2={user_2} my_id={my_id} key="topartists"/>
+                    <Trendex user_1={user_1} user_2={user_2} my_id={my_id} key="trendex"/>
+                    <TopGenres user_1={user_1} user_2={user_2} key="topgenres"/>
+                    <MusicalAttr user_1={user_1} user_2={user_2} my_id={my_id} key="musicalattr"/>
+                    <Decades user_1={user_1} user_2={user_2} my_id={my_id} key="decades"/> 
+                    <PlaylistGenerator user_1={user_1} user_2={user_2} my_id={my_id} key="playlistgenerator"/>
+                    <Members user_1={user_1} user_2={user_2} my_id={my_id} key="membersscreen"/>
+                    <Share history={history} key="sharepage" />
+                </SwipeableViews>
+            );
+        } else {
+            return (
+                <SwipeableViews>
+                    <TopArists user_1={user_1} user_2={user_2} my_id={my_id} key="topartists"/>
+                    <Trendex user_1={user_1} user_2={user_2} my_id={my_id} key="trendex"/>
+                    <TopGenres user_1={user_1} user_2={user_2} key="topgenres"/>
+                    <MusicalAttr user_1={user_1} user_2={user_2} my_id={my_id} key="musicalattr"/>
+                    <Decades user_1={user_1} user_2={user_2} my_id={my_id} key="decades"/> 
+                    <PlaylistGenerator user_1={user_1} user_2={user_2} my_id={my_id} key="playlistgenerator"/>
+                    <Share history={history} key="sharepage" />
+                </SwipeableViews>
+            );
+        }
+    } else {
         return <div/>
     }
 }
