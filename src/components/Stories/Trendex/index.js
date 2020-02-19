2918786aa341c25159e0c1947d9429cbe678071a
himadri narasimhamurthy <himadri99@gmail.com>
2020-02-19 11:54:21 -0500

    fixed styling on 1 2 and 5 and added popups

--- src/components/Stories/Trendex/index.js
+++ src/components/Stories/Trendex/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Page from '../../Page';
 import { User } from '../../../types';
 import { string } from 'prop-types';
+import Collapsible from 'react-collapsible';
 
 const Trendex = ({ user_1, user_2, my_id }) => {
     const { trendex: user1Trendex, display_name: user1Name } = user_1;
@@ -12,23 +13,31 @@ const Trendex = ({ user_1, user_2, my_id }) => {
     const renderPopup = () => {
         return (
             <div id="PopupBackground">
-                <button>X</button>                
-                <h1>About Top Artists</h1>
-                <h2> What am I looking at?</h2>
-                <p>This all has to get filled in- maybe other questions headers too idk- doesn’t always have to be “what am i looking at?</p>
-                <h1>About Duet</h1>
-                <h2>What information does Duet have?</h2>
-                <p>Duet only stores information given to us from Spotify: your username and your top artists/songs- that’s it! As much as we would like your credit card information or email and password- Spotify’s authentication process prevents us. Dang!</p>
-                <h2>How does Duet work?</h2>
-                <p>Once you’ve logged in through Spotify’s website and selected a person or friend to compare with, Duet receives the top fifty artists/songs from the past six months of yourself and whoever you chose to compare with. From there, using Spotify’s public api, we look at all the metadata of your music to show how it contrasts! We can even generate playlists to blend your two music tastes like a tropical smoothie on a hot day. Neato!</p>
-                <h2>Who made Duet?</h2>
-                <p>Duet was made by group of collegiate seniors as a capstone project. We would have been something that cures global warming (easy) or systemic racism and misogyny (child’s play), but unfortunately we were bound by an ancient form of magic known as onomancy, the arcane school of names, to Xolgoth- Daedric prince of the seventh layer of hell/Duet marketing lead. </p>
-                <h2>Why is it mobile only?</h2>
-                <p>Listen here you little shit...</p>
-                <h2>Contact us</h2>
-                <p>Have any more questions? You can send us emails at duetwastaken@gmail.com.</p>
-                <h2>Donate!</h2>
-                <p>To keep Duet ad free, we have been paying for hosting out of pocket :((  consider donating to my venmo: @nicBerg... jk.... unless?</p>
+                <button onClick={() => setShowPopup(!showPopup)}>X</button>                
+                <Collapsible className="topHeader" openedClassName="topHeader" trigger="About Top Artists" open="true">
+                    <h2> What am I looking at?</h2>
+                    <p>This all has to get filled in- maybe other questions headers too idk- doesn’t always have to be “what am i looking at?</p>
+                </Collapsible>
+                <Collapsible className="topHeader" openedClassName="topHeader" trigger="About Duet" open="true">
+                    <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="What information does Duet have?">
+                        <p>Duet only stores information given to us from Spotify: your username and your top artists/songs- that’s it! As much as we would like your credit card information or email and password- Spotify’s authentication process prevents us. Dang!</p>
+                    </Collapsible>
+                    <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="How does Duet work?">
+                        <p>Once you’ve logged in through Spotify’s website and selected a person or friend to compare with, Duet receives the top fifty artists/songs from the past six months of yourself and whoever you chose to compare with. From there, using Spotify’s public api, we look at all the metadata of your music to show how it contrasts! We can even generate playlists to blend your two music tastes like a tropical smoothie on a hot day. Neato!</p>
+                    </Collapsible>
+                    <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Who made Duet?">
+                        <p>Duet was made by group of collegiate seniors as a capstone project. We would have been something that cures global warming (easy) or systemic racism and misogyny (child’s play), but unfortunately we were bound by an ancient form of magic known as onomancy, the arcane school of names, to Xolgoth- Daedric prince of the seventh layer of hell/Duet marketing lead. </p>
+                    </Collapsible>
+                    <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Why is it mobile only?">
+                        <p>Listen here you little shit...</p>
+                    </Collapsible>
+                    <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Contact us">
+                        <p>Have any more questions? You can send us emails at duetwastaken@gmail.com.</p>
+                    </Collapsible>
+                    <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Donate!">
+                        <p>To keep Duet ad free, we have been paying for hosting out of pocket :((  consider donating to my venmo: @nicBerg... jk.... unless?</p>
+                    </Collapsible>
+                </Collapsible>
             </div>
         )
     }
@@ -83,7 +92,7 @@ const Trendex = ({ user_1, user_2, my_id }) => {
                 </div>
 
                 { showPopup &&
-                    <div className="popupInfo" onClick={handleInfoClick}>
+                    <div className="popupInfo">
                         {renderPopup()}
                     </div>
                 }
