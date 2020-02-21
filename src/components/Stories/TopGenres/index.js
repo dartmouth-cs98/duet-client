61e41c42f08ffa6b072ceaafcb12282bc2a52fcc
nicBerg <nicolas.j.bergen.20@dartmouth.edu>
2020-02-21 14:21:34 -0500

    bubble fix

--- src/components/Stories/TopGenres/index.js
+++ src/components/Stories/TopGenres/index.js
@@ -21,21 +21,53 @@ function toDegrees(radians) {return radians * (180 / Math.PI)}
 function splitRange(angle_range, radius, angle, s) {return [[angle_range[0], angle - s/radius], [angle + s/radius, angle_range[1]]]}
 
 
+class Bubble {
+    constructor(label, r, x, y, c) {
+        this.label = label;
+        this.r = r;
+        this.x = x;
+        this.y = y;
+        this.c = c;
+        
+        this.angle_ranges;
+        this.parent = [];
+        this.children = [];
+    }
+
+    split_range(angle, s) {
+        var range;
+        for (var i = 0; i < this.angle_ranges.length; i++) if (this.angle_ranges[i][0] < angle < this.angle_ranges[i][1]) range = i;
+        var split_range = this.angle_ranges.splice(range, 1);
+        this.angle_ranges.push([split_range[0], angle - s/this.r], [angle + s/this.r, split_range[1]])
+    }
+
+    correct_center() {
+        this.x -= this.r;
+        this.y -= this.r;
+    }
+
+    
+
+
+}
+
+
+
+
 const Bubbles = ({ topGenres, name, bubbleColor, width, height }) => {
     const pi = Math.PI;
     const t = 2 * pi;
     const A = pi * Math.pow((height * .5), 2);
     var bubbles = [];
-    console.log(topGenres);
 
     var total_count = 0;
     topGenres.forEach((genre) => {total_count += genre.count;});
-    console.log(total_count);
 
     topGenres.forEach((genre) => {
         const { label, count } = genre
         const percentage = (count / total_count);
-        bubbles = [...bubbles, {genre: label, r: Math.sqrt((A*percentage))/pi, x: 0, y: 0, c: [], a: [], pa: 0}];
+        //bubbles = [...bubbles, {genre: label, r: Math.sqrt((A*percentage))/pi, x: 0, y: 0, c: [], a: [], pa: 0}];
+        bubbles = [...bubbles, new Bubble(label, Math.sqrt((A*percentage))/pi, 0, 0)]; 
     });
     
     bubbles.sort(function(a, b) {return b.r - a.r})
@@ -46,48 +78,22 @@ const Bubbles = ({ topGenres, name, bubbleColor, width, height }) => {
     for (var i = 1; i < bubbles.length; i++){
         var b1 = bubbles[i];
         var b0 = bubbles[randRangeInt(0, Math.floor(i/4))];
-        const angle_range_i = randRangeInt(0, b0.a.length - 1)
-        const a = randRange(b0.a[angle_range_i][0], b0.a[angle_range_i][1]);
-        //console.log(toDegrees(a));
+        const angle_range_i = randRangeInt(0, b0.angle_range.length - 1)
+        const a = randRange(b0.angle_range[angle_range_i][0], b0.angle_range[angle_range_i][1]);
         const d = b0.r + b1.r * .6;
         b1.a.push([a - toRadians(90), a + toRadians(90)]);
-        //const new_range = splitRange(b0.a[angle_range_i]);
-        //b0.a.push(new_range[0], new_range[1]);
-        //b0.angle_range
-        //var a = Math.random()*t;
         b1.x = b0.x + d * Math.cos(a);
         b1.y = b0.y + d * Math.sin(a);
     }
 
-    for (var i = 0; i < bubbles.length; i++){
-        var b = bubbles[i];
-        b.x -= b.r;
-        b.y -= b.r;
-    }
-
-    // var cx = 0;
-    // var cy = 0;
-    // bubbles.forEach((bubble) => {
-    //     cx += bubble.x;
-    //     cy += bubble.y;
-    // });
-    // cx /= bubbles.length;
-    // cy /= bubbles.length;
-
-    // bubbles.forEach((bubble) => {
-    //     // bubble.x -= cx;
-    //     // bubble.y -= cy;
-    // });
-
-    //const RenderBubble({ })
+    for (var i = 0; i < bubbles.length; i++) bubbles[i].correct_center();
 
     return (
         <div className="Bubbles">
             {bubbles.map((bubble) => {
-                const { genre, r, x, y } = bubble;
                 return (
-                    <div className="Bubble" style={{ width: r*2, height: r*2, transform: `translate(${x}px, ${y}px)` }} >
-                        <h1 className="Bubble-Label" style={{ fontSize: 10 }}>{genre}</h1>
+                    <div key={name+bubble.label} className="Bubble" style={{ width: bubble.r*2, height: bubble.r*2, transform: `translate(${bubble.x}px, ${bubble.y}px)` }} >
+                        <h1 className="Bubble-Label" style={{ fontSize: 10 }}>{bubble.label}</h1>
                         {/* {$("h1").textfit('bestfit')} */}
                         <div className={`Bubble-Image-${bubbleColor}`}></div>   
                           
