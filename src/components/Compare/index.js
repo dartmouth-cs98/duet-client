886b1d43d205e9d50842b2270d3bf8cceada1e8e
Zachary Gottschall <zachary.m.gottschall.20@dartmouth.edu>
2020-02-25 15:31:04 -0500

    fix nic's mistakes ;P

--- src/components/Compare/index.js
+++ src/components/Compare/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,6 +7,7 @@ import DuetLogo from '../DuetLogo';
 import Button from '../Button';
 import Switch from '../Switch';
 import Search from '../Search';
+import ModalWrapper from '../Modal';
 import { fetchUser1, fetchUser2 } from '../../actions';
 import { addGroup } from '../../utils/backendUtils';
 import { func } from 'prop-types';
@@ -14,11 +16,30 @@ const LOGO_HEIGHT = 130;
 const LOGO_WIDTH = 210;
 const EVERYONE_ID = "Everyone";
 
+const CreateGroupModal = ({ toggleModal }) => {
+    return (
+        <div className="Compare-CreateGroup-Modal">
+            <div className="Compare-CreateGroup-Modal-Close">
+                <button onClick={toggleModal}>
+                    <svg width="20" height="20" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 1L16.5 16.5" stroke="white" strokeWidth="2" strokeLinecap="round"/>
+                        <path d="M16.5 1L0.999999 16.5" stroke="white" strokeWidth="2" strokeLinecap="round"/>
+                    </svg>  
+                </button>  
+            </div>
+            <>  
+                <h1>Name your group</h1>
+                {/* <ReactLoading type={'bars'} color="#fff" /> */}
+            </>
+        </div>
+    )
+}
+
 const Compare = ({ history }) => {
 
     const dispatch = useDispatch();
     const { user_1, my_id } = useSelector((state) => state.users);
-    const [showPopup, setShowPopup] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     const [topIsSearching, setTopIsSearching] = useState(false);
     const [topUser, setTopUser] = useState({ name: 'Me', id: my_id });
@@ -29,36 +50,29 @@ const Compare = ({ history }) => {
     const [isComparing, setIsComparing] = useState(true);
     const [isMixing, setIsMixing] = useState(true);
 
-    const [groupNameVal, setGroupNameVal] = useState('');
+    // const [groupNameVal, setGroupNameVal] = useState('');
 
-    const renderPopup = () => {
-        return (
-            <div id="PopupBackground">
-                <button id="close" onClick={() => handleClose()}>x</button>
-                <h3>name your group</h3>
-                <input type="text" value={groupNameVal} onChange={handleGroupNameChange}></input>
-                <button onClick={() => handleAddGroupClick()}>create</button>
-            </div>
-        )
+    // const handleClose = () => {
+    //     if (showPopup) {
+    //         setShowPopup(!showPopup);
+    //     } 
+    // }
+
+    // const handleAddGroupClick = () => {
+    //     addGroup(groupNameVal, user_1.id)
+    //     if (showPopup) {
+    //         setShowPopup(!showPopup);
+    //     } 
+    // }
+
+    // const handleGroupNameChange = (e) => {
+    //     const { value } = e.target;
+    //     setGroupNameVal(value)
+    // };
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
     }
-
-    const handleClose = () => {
-        if (showPopup) {
-            setShowPopup(!showPopup);
-        } 
-    }
-
-    const handleAddGroupClick = () => {
-        addGroup(groupNameVal, user_1.id)
-        if (showPopup) {
-            setShowPopup(!showPopup);
-        } 
-    }
-
-    const handleGroupNameChange = (e) => {
-        const { value } = e.target;
-        setGroupNameVal(value)
-    };
 
     const handleGoClick = () => {
         dispatch(fetchUser1(topUser.id))
@@ -77,62 +91,59 @@ const Compare = ({ history }) => {
     }
 
     return (
-        <Page background={'#212034'}>
-            <Search 
-                enabled={topIsSearching || bottomIsSearching} 
-                setIsSearching={topIsSearching ? setTopIsSearching : setBottomIsSearching}
-                setUser={topIsSearching ? setTopUser : setBottomUser}
-            />
-            { !topIsSearching && !bottomIsSearching &&
-                <div className="Compare-Page">
-                    <DuetLogo width={LOGO_WIDTH} height={LOGO_HEIGHT}/>
-                    <div className="Compare-Inputs">
-                        <div className="Compare-Input">
-                            <button onClick={() => setTopIsSearching(true)}>{topUser.name}</button>
-                        </div>
-                        <h1>+</h1>
-                        <div className="Compare-Input">
-                            <button onClick={() => setBottomIsSearching(true)}>{bottomUser.name}</button>
-                        </div>
-                        <div className="Compare-ShareDuet">can&apos;t find your friend? <u>share duet!</u></div>
-                    </div>
-                    <div className="Compare-Toggles">
-                        <div className="Compare-Toggle">
-                            <div className="Compare-Toggle-Description">
-                                <h1>taste comparisons</h1>
-                                <h2>includes a series of infographics comparing the two chosen in your Duet</h2>
+        <ModalWrapper showModal={showModal}>
+            <CreateGroupModal toggleModal={toggleModal} />
+            <Page background={'#212034'}>
+                <Search 
+                    enabled={topIsSearching || bottomIsSearching} 
+                    setIsSearching={topIsSearching ? setTopIsSearching : setBottomIsSearching}
+                    setUser={topIsSearching ? setTopUser : setBottomUser}
+                />
+                { !topIsSearching && !bottomIsSearching &&
+                    <div className="Compare-Page">
+                        <DuetLogo width={LOGO_WIDTH} height={LOGO_HEIGHT}/>
+                        <div className="Compare-Inputs">
+                            <div className="Compare-Input">
+                                <button onClick={() => setTopIsSearching(true)}>{topUser.name}</button>
                             </div>
-                            <Switch
-                                isOn={isComparing}
-                                id={"comparing"}
-                                handleToggle={handleToggleComparing}
-                            />
-                        </div>
-                        <div className="Compare-Toggle">
-                            <div className="Compare-Toggle-Description">
-                                <h1>music taste mixer</h1>
-                                <h2>includes a tool that creates a Spotify playlist that is a mix of taste between the two chosen in your Duet</h2>
+                            <h1>+</h1>
+                            <div className="Compare-Input">
+                                <button onClick={() => setBottomIsSearching(true)}>{bottomUser.name}</button>
                             </div>
-                            <Switch
-                                isOn={isMixing}
-                                id={"mixing"}
-                                handleToggle={handleToggleMixing}
-                            />
+                            <div className="Compare-ShareDuet">can&apos;t find your friend? <u>share duet!</u></div>
                         </div>
-                    </div>
-                    <div className="Compare-Buttons">
-                        <Button onClick={handleGoClick} width={178} >go!</Button>
-                        <button className="Compare-CreateGroup" onClick={() => setShowPopup(!showPopup)}>or <u>create a group!</u></button>
-                    </div>
-    
-                    { showPopup &&
-                        <div className="popup">
-                            {renderPopup()}
+                        <div className="Compare-Toggles">
+                            <div className="Compare-Toggle">
+                                <div className="Compare-Toggle-Description">
+                                    <h1>taste comparisons</h1>
+                                    <h2>includes a series of infographics comparing the two chosen in your Duet</h2>
+                                </div>
+                                <Switch
+                                    isOn={isComparing}
+                                    id={"comparing"}
+                                    handleToggle={handleToggleComparing}
+                                />
+                            </div>
+                            <div className="Compare-Toggle">
+                                <div className="Compare-Toggle-Description">
+                                    <h1>music taste mixer</h1>
+                                    <h2>includes a tool that creates a Spotify playlist that is a mix of taste between the two chosen in your Duet</h2>
+                                </div>
+                                <Switch
+                                    isOn={isMixing}
+                                    id={"mixing"}
+                                    handleToggle={handleToggleMixing}
+                                />
+                            </div>
                         </div>
-                    }
-                </div>   
-            }
-        </Page>
+                        <div className="Compare-Buttons">
+                            <Button onClick={handleGoClick} width={178} >go!</Button>
+                            <button className="Compare-CreateGroup" onClick={toggleModal}>or <u>create a group!</u></button>
+                        </div>
+                    </div>   
+                }
+            </Page>
+        </ModalWrapper>
     )
 }
 
