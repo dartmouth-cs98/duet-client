2cee56116e57d9f38d69de48c165a71e3284da37
Himadri Narasimhamurthy <himadri99@gmail.com>
2020-03-01 20:09:30 -0500

    fixed learn more pop-up (#134)

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -11,6 +11,7 @@ import { getToken } from '../../utils/tokenUtils';
 import { useDispatch } from 'react-redux';
 import { func } from 'prop-types';
 import { joinGroup } from '../../utils/backendUtils';
+import Collapsible from 'react-collapsible';
 
 const LOGO_HEIGHT = 150;
 const LOGO_WIDTH = 240;
@@ -66,17 +67,46 @@ const Login = ({ history, match }) => {
 
     const renderPopup = () => {
         return (
-            <div id="PopupBackground">
-                <p>We are a group of college people doing stuff. We are not professionals so don&apos;t treat us as such. Thanks for clicking this teeny button.</p>
-                <p id="close">click anywhere to close</p>
-            </div>
+            <div id="PopupOuter">
+                    <div id="blur"></div>
+                    <div className="Popup">
+                        <script defer src="https://use.fontawesome.com/releases/v5.12.0/js/all.js"></script>
+                        <div className="Popup-Close-Button">
+                            <button onClick={() => setShowPopup(!showPopup)}>
+                                <svg width="10" height="10" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 1L16.5 16.5" stroke="white" strokeWidth="2" strokeLinecap="round"/>
+                                    <path d="M16.5 1L0.999999 16.5" stroke="white" strokeWidth="2" strokeLinecap="round"/>
+                                </svg>  
+                            </button>        
+                        </div>      
+                        <h1>FAQ</h1>    
+                        <Collapsible className="topHeader" openedClassName="topHeader" trigger={`What does Duet do?`} open="true">
+                            <h2> What am I looking at?</h2>
+                            <p>Duet leverages public Spotify API data, meaning your top 50 songs and top 50 artists - nothing else! It then allows you to visually compare your music with the music of other Duet users. Duet also offers a tool for you to mix your music tastes with your friends or with groups.</p>
+                        </Collapsible>
+                        <Collapsible className="topHeader" openedClassName="topHeader" trigger="About Duet" open="true">
+                            <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="What information does Duet have?">
+                                <p>Duet only stores information given to us from Spotify: your username and your top artists/songs- that’s it! As much as we would like your credit card information or email and password- Spotify’s authentication process prevents us. Dang!</p>
+                            </Collapsible>
+                            <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="How does Duet work?">
+                                <p>Once you’ve logged in through Spotify’s website and selected a person or friend to compare with, Duet receives the top fifty artists/songs from the past six months of yourself and whoever you chose to compare with. From there, using Spotify’s public api, we look at all the metadata of your music to show how it contrasts! We can even generate playlists to blend your two music tastes like a tropical smoothie on a hot day. Neato!</p>
+                            </Collapsible>
+                            <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Who made Duet?">
+                                <p>Duet was made by group of collegiate seniors as a capstone project. We would have been something that cures global warming (easy) or systemic racism and misogyny (child’s play), but unfortunately we were bound by an ancient form of magic known as onomancy, the arcane school of names, to Xolgoth- Daedric prince of the seventh layer of hell/Duet marketing lead. </p>
+                            </Collapsible>
+                            <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Why is it mobile only?">
+                                <p>Listen here you little shit...</p>
+                            </Collapsible>
+                            <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Contact us">
+                                <p>Have any more questions? You can send us emails at duetwastaken@gmail.com.</p>
+                            </Collapsible>
+                            <Collapsible className='innerCollapsible' openedClassName='innerCollapsible' trigger="Donate!">
+                                <p>To keep Duet ad free, we have been paying for hosting out of pocket :((</p>
+                            </Collapsible>
+                        </Collapsible>
+                    </div>
+                </div>
         )
-    }
-
-    const handleClick = () => {
-        if (showPopup) {
-            setShowPopup(!showPopup);
-        } 
     }
 
     return (
@@ -91,8 +121,8 @@ const Login = ({ history, match }) => {
                         <h2 className="Login-description">what duet does: <br/> visually compares your music taste with others generates playlists to mix your taste with others</h2>
                     </div>
                     <button className="Login-LearnMore" onClick={() => setShowPopup(!showPopup)}>learn more</button>
-                    { showPopup &&
-                        <div className="popup" onClick={handleClick}>
+                    {showPopup &&
+                        <div className="popup">
                             {renderPopup()}
                         </div>
                     }
